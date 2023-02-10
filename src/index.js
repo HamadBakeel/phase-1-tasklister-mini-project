e28df8e53b967e474139586c34c9fe05a8f1e40d
Hamad Bakeel <90466313+HamadBakeel@users.noreply.github.com>
2023-02-10 20:52:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    document.forms[0].addEventListener('submit',function (e){
+        e.preventDefault();
+        const taskText = document.getElementById('new-task-description').value;
+        const tasksContainer = document.getElementById('tasks');
+        const task = document.createElement('div')
+        task.textContent = taskText;
+        tasksContainer.appendChild(task)
+
+    })
 });
